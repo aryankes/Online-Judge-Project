cd96e1bc0a6d0d78186775f3a5fc8b54db06ce57
aryankes <159420292+aryankes@users.noreply.github.com>
2024-05-23 21:01:03 +0530

    new

--- AuthFinal/backend/controllers/exampleController.js
+++ AuthFinal/backend/controllers/exampleController.js
@@ -13,10 +13,17 @@ exports.b = async (req, res) => {
 exports.register = async (req, res) => {
   
   try {
+    //get all the data from the frontend
     const {firstName,lastName,userhandle,email,password}=req.body;
+
+    //check that all the data should exist
     if(!(firstName&&lastName&&userhandle&&email&&password)){
         return res.status(400).send("Please enter all the information");
     }
+
+    //add more valiidations/constraints
+
+    //check for existing userhandle and email
     const existingUser=await User.findOne({ userhandle });
     if(existingUser){
         return res.status(400).send("User already exists with the same handle");
@@ -25,17 +32,24 @@ exports.register = async (req, res) => {
     if(existingUser2){
         return res.status(400).send("User already exists with the same email");
     }
+    
+    //creating a hashed password
     const hashedPassword= await bcrypt.hash(password,10);
+
+    //save the user in DB
     const user=await User.create({
         firstName,
         lastName,
         userhandle,
         email,
-        password: hashedPassword,
+        password: hashedPassword,//saving hashed password for increasing security
     });
+
+    //generate a token for the user and send it
     const token=jwt.sign({id:user._id,userhandle},process.env.SECRET_KEY,{
         expiresIn: '1h',
     });
+    //passing the token to user
     user.token=token;
     user.password=undefined;
     res.status(200).json({message: "You have succesfully registerd!",user})
@@ -45,13 +59,53 @@ catch (error) {
 }
 };
 
-// // POST a new example
-// exports.createExample = async (req, res) => {
-//   const newExample = new Example(req.body);
-//   try {
-//     const savedExample = await newExample.save();
-//     res.status(201).json(savedExample);
-//   } catch (err) {
-//     res.status(400).json({ message: err.message });
-//   }
-// };
+exports.login= async(req,res)=>{
+try {
+    //get all the data from frontend
+    const {line,password}=req.body;
+    if(!(line&&password)){
+        return res.status(400).send("Please enter all the information");
+    }
+
+    //add more validations
+
+    //find the user in the db
+    const user= await User.findOne({line});
+    if(!user){
+        // user=await User.findOne({line});
+        if(!user){
+            return res.status(404).send("User not found");
+        }
+    }
+     
+    //comparing the password
+    const enteredPassword=bcrypt.compare(password,user.password);
+    if(!enteredPassword){
+        return res.status(404).send("Password does not match");
+    }
+
+    //token creation
+    const token=jwt.sign({id:user._id},procees.env.SECRET_KEY,{
+        expiresIn:"1h"
+    });
+    user.token=token;
+    user.password=undefined;
+
+    //storing token in cookies with option
+    const options={
+        expiresIn:new Date(Date.now()+1*24*60*60*1000),
+        httpOnly: true,//only manipulted by server not by your client/frontend 
+    }
+
+    //send the token
+    res.status(200).cookie("token",token,options).json({
+        message:"You have succeddfully logged in!",
+        success: true,
+        token,
+    });
+} catch (error) {
+    console.log(error);
+}
+    
+}
+
--- AuthFinal/backend/routes/auth_routes.js
+++ AuthFinal/backend/routes/auth_routes.js
@@ -5,4 +5,6 @@ const exampleController = require('../controllers/exampleController.js');
 router.get('/a', exampleController.a);
 router.get('/b', exampleController.b);
 router.post('/register', exampleController.register);
+router.post('/login', exampleController.login);
+
 module.exports = router;
