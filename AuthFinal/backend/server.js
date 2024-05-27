ae52348d2da036b7431d47a65a450e33cf8e47c1
aryankes <159420292+aryankes@users.noreply.github.com>
2024-05-27 20:14:45 +0530

    firstcommit

--- AuthFinal/backend/server.js
+++ AuthFinal/backend/server.js
@@ -5,7 +5,7 @@ const cookieParser=require("cookie-parser");
 // const cors = require('cors');
 
 // Load environment variables
-dotenv.config();
+dotenv.config({path: '../../.env'});
 
 // Initialize Express
 const app = express();
