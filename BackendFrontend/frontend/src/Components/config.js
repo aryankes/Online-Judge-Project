23945c5e13e957e1efdaf11fb98ae238e828b7d4
aryankes <159420292+aryankes@users.noreply.github.com>
2024-06-19 03:28:33 +0530

    https related changes

--- BackendFrontend/frontend/src/Components/config.js
+++ BackendFrontend/frontend/src/Components/config.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = `${import.meta.env.VITE_REACT_INSTANCE_IP}:5000`;
-const API_COMPILER_URL = `${import.meta.env.VITE_REACT_INSTANCE_IP}:5000`;
+const API_BASE_URL = `${import.meta.env.VITE_REACT_INSTANCE_IP}`;
+const API_COMPILER_URL = `${import.meta.env.VITE_REACT_INSTANCE_IP}`;
 
 
 export { API_BASE_URL ,API_COMPILER_URL};
